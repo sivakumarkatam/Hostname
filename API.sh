449ffbd1442aa5fe654cfef10d6d165bc4ae5f9c
sivakumarkatam <siva.katam@blazeclan.com>
2017-12-26 16:40:01 +0530

    Update API.sh

--- API.sh
+++ API.sh
@@ -9,7 +9,7 @@ sudo yum -y install wget rpm epel-release awscli ruby python
 
 # sudo yum -y localinstall jre-8u131-linux-x64.rpm
 #preserve host name
-echo "preserve_hostname: true" >> /etc/cloud/cloud.cfg
+#echo "preserve_hostname: true" >> /etc/cloud/cloud.cfg
 
 # sudo rm -rf jre-8u131-linux-x64.rpm
 sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jre-8u151-linux-x64.tar.gz"
